c11245490aefb224f9dd178ff52cf548c9b09526
serge <serguzeo@gmail.com>
2023-09-06 19:04:32 +0300

    Добавил Swagger для автодокументации. Добавил API с доступом к конретному тьютору. Переименовал папку Moblen -> conf, чтобы название отражало суть. Добавлено несколько зависимостей в requirements. Положено начало для авторизации пользователей в API для доступа к контенту

--- Moblen/conf/asgi.py
+++ Moblen/conf/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI Moblen for Moblen project.
+ASGI conf for conf project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Moblen.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings')
 
 application = get_asgi_application()
